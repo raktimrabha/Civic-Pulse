c85d607205163fecc9b6b9434c6e0b57a28444aa
rktm <rabharaktim@outlook.com>
2025-04-12 13:33:37 +0530

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react' // Changed from plugin-react-swc
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
